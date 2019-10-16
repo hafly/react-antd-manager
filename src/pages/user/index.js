42b4a6cdf9c3b94441448f9e2474c75fd377e545
hafly <188150920@qq.com>
2019-10-16 15:58:53 +0800

    员工管理-添加员工

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, Form, Button, Table, Input, Radio, Select, DatePicker, Spin, Modal, message} from 'antd';
+import {Card, Form, Button, Input, Radio, Select, DatePicker, Spin, Modal, message} from 'antd';
 import BaseForm from '../../components/BaseForm';
 import BaseTable from '../../components/BaseTable';
 import axios from '../../utils/axios';
@@ -7,9 +7,11 @@ import utils from '../../utils/utils';
 import dictionary from '../../config/dictionary';
 
 const FormItem = Form.Item;
+const TextArea = Input.TextArea;
+const RadioGroup = Radio.Group;
 const Option = Select.Option;
 
-export default class City extends React.Component {
+export default class User extends React.Component {
     state = {
         loading: true,
         dataSource: [],
@@ -101,6 +103,7 @@ export default class City extends React.Component {
         this.requestList();
     }
 
+    // 请求表格数据
     requestList() {
         let self = this;
         this.setState({
@@ -127,20 +130,41 @@ export default class City extends React.Component {
         });
     }
 
+    // 表单查询
     handleFormSubmit = (params) => {
         this.params = params;
         this.params.page = 1;
         this.requestList();
     }
 
-    handleOperate = (type) => {
+    // 员工操作
+    showModalOperate = (type) => {
         if (type === 'add') {
             this.setState({
                 type,
                 isVisibleModal: true,
-                title: '创建员工'
+                title: '添加员工'
             })
         }
+    }
+
+    // 员工操作确认
+    showModalOperateOk = () => {
+        this.userForm.props.form.validateFields((err, fieldsValue) => {
+            if (!err) {
+                axios.ajax({
+                    url: '/user/add',
+                    data: fieldsValue
+                }).then((res) => {
+                    message.success(res.message);
+                    this.setState({
+                        isVisibleModal: false
+                    });
+                    this.userForm.props.form.resetFields();
+                    this.requestList();
+                });
+            }
+        });
     }
 
     onRadioSelect = (selectedRows) => {
@@ -154,10 +178,10 @@ export default class City extends React.Component {
                     <BaseForm formList={this.formList} formSubmit={this.handleFormSubmit}/>
                 </Card>
                 <Card className="opertate-wrap" style={{marginTop: 10, borderBottom: 0}}>
-                    <Button type="primary" icon="plus" onClick={() => this.handleOperate('add')}>创建员工</Button>
-                    <Button type="primary" icon="edit" onClick={() => this.handleOperate('edit')}>编辑员工</Button>
-                    <Button type="primary" onClick={() => this.handleOperate('detail')}>员工详情</Button>
-                    <Button type="primary" icon="delete" onClick={() => this.handleOperate('delete')}>删除员工</Button>
+                    <Button type="primary" icon="plus" onClick={() => this.showModalOperate('add')}>添加员工</Button>
+                    <Button type="primary" icon="edit" onClick={() => this.showModalOperate('edit')}>编辑员工</Button>
+                    <Button type="primary" onClick={() => this.showModalOperate('detail')}>员工详情</Button>
+                    <Button type="primary" icon="delete" onClick={() => this.showModalOperate('delete')}>删除员工</Button>
                 </Card>
                 <div className="content-wrap" style={{borderTop: 0}}>
                     <BaseTable
@@ -178,19 +202,18 @@ export default class City extends React.Component {
                             isVisibleModal: false
                         })
                     }}
-                    onOk={this.handleSubmit}
+                    onOk={this.showModalOperateOk}
                 >
-                    <OpenCityForm wrappedComponentRef={(inst) => {
-                        this.cityForm = inst
+                    <UserForm wrappedComponentRef={(inst) => {
+                        this.userForm = inst
                     }}/>
                 </Modal>
-
             </Spin>
         )
     }
 }
 
-class OpenCityForm extends React.Component {
+class UserForm extends React.Component {
     render() {
         const {getFieldDecorator} = this.props.form;
         const formItemLayout = {
@@ -199,40 +222,56 @@ class OpenCityForm extends React.Component {
         }
         return (
             <Form layout="horizontal">
-                <FormItem label="选择城市" {...formItemLayout}>
+                <FormItem label="用户名" {...formItemLayout}>
                     {
-                        getFieldDecorator('city_id', {
-                            initialValue: '1'
+                        getFieldDecorator('user_name', {
+                            initialValue: '',
+                            rules: [
+                                {required: true, message: '用户名不能为空!'}
+                            ]
+                        })(
+                            <Input type="text" placeholder="请输入用户名" autoComplete="off"/>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="性别" {...formItemLayout}>
+                    {
+                        getFieldDecorator('sex', {
+                            initialValue: 1
+                        })(
+                            <RadioGroup>
+                                <Radio value={1}>男</Radio>
+                                <Radio value={2}>女</Radio>
+                            </RadioGroup>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="状态" {...formItemLayout}>
+                    {
+                        getFieldDecorator('state', {
+                            initialValue: 1
                         })(
                             <Select>
-                                <Option value="">全部</Option>
-                                <Option value="1">北京市</Option>
-                                <Option value="2">天津市</Option>
+                                <Option value={1}>咸鱼一条</Option>
+                                <Option value={2}>风华浪子</Option>
+                                <Option value={3}>北大才子</Option>
+                                <Option value={4}>百度FE</Option>
+                                <Option value={5}>创业者</Option>
                             </Select>
                         )
                     }
                 </FormItem>
-                <FormItem label="运营模式" {...formItemLayout}>
+                <FormItem label="生日" {...formItemLayout}>
                     {
-                        getFieldDecorator('op_mode', {
-                            initialValue: '1'
-                        })(
-                            <Select>
-                                <Option value="1">自营</Option>
-                                <Option value="2">加盟</Option>
-                            </Select>
+                        getFieldDecorator('birthday')(
+                            <DatePicker/>
                         )
                     }
                 </FormItem>
-                <FormItem label="用车模式" {...formItemLayout}>
+                <FormItem label="联系地址" {...formItemLayout}>
                     {
-                        getFieldDecorator('use_mode', {
-                            initialValue: '1'
-                        })(
-                            <Select>
-                                <Option value="1">指定停车点</Option>
-                                <Option value="2">禁停区</Option>
-                            </Select>
+                        getFieldDecorator('address')(
+                            <TextArea rows={3} placeholder="请输入联系地址"/>
                         )
                     }
                 </FormItem>
@@ -241,4 +280,4 @@ class OpenCityForm extends React.Component {
     }
 }
 
-OpenCityForm = Form.create()(OpenCityForm);
+UserForm = Form.create()(UserForm);
